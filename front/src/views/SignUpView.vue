a734480f5c565cd1377ed131d6ec68d7f0aa9708
leejiun <leejiwoo0126@gmail.com>
2025-05-22 14:24:39 +0900

    update

--- front/src/views/SignUpView.vue
+++ front/src/views/SignUpView.vue
@@ -1,11 +1,39 @@
 <template>
 	<div>
 		<h1>회원가입 페이지</h1>
+		<form @submit.prevent="onSignup">
+			<label for="username">username : </label>
+			<input type="text" id="username" v-model="username">
+			<br>
+			<label for="password1">password1 : </label>
+			<input type="password1" id="password1" v-model="password1">
+			<br>
+			<label for="password2">password2 : </label>
+			<input type="password2" id="password2" v-model="password2">
+			<input type="submit" value="signup">
+			<br>
+		</form>
 	</div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import { useAccountStore } from '@/stores/accounts'
 
+const username = ref('')
+const password1 = ref('')
+const password2 = ref('')
+
+const store = useAccountStore()
+const onSignup = function() {
+    const userInfo = {
+        username : username.value,
+        password1 : password1.value,
+        password2 : password2.value,
+    }
+
+    store.createUser(userInfo)
+}
 </script>
 
 <style scoped>
