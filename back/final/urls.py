83e433900a0635b3a49fee544f40014d666a4a21
sunshinemoon <sunshinemoongit@gmail.com>
2025-05-22 16:24:02 +0900

    dev saving api

--- back/final/urls.py
+++ back/final/urls.py
@@ -7,12 +7,9 @@ from drf_yasg import openapi
 # Swagger 스키마 뷰 설정
 schema_view = get_schema_view(
     openapi.Info(
-        title="내 프로젝트 API 문서",
-        default_version='v1',
-        description="API 명세서",
-        terms_of_service="https://www.example.com/terms/",
-        contact=openapi.Contact(email="contact@example.com"),
-        license=openapi.License(name="MIT License"),
+        title="MoneyUp API",
+        default_version="v1",
+        description="금융 상품 비교 플랫폼",
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
@@ -29,12 +26,14 @@ urlpatterns = [
     path("dj-rest-auth/signup/", include("dj_rest_auth.registration.urls")),
     # swagger
     # Swagger UI
-    path("swagger/", schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
     # ReDoc
-    path("redoc/", schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
     # JSON/YAML 형태로도 제공
-    path("swagger.json", schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path("swagger.yaml", schema_view.without_ui(cache_timeout=0), name='schema-yaml'),
+    path("swagger.json", schema_view.without_ui(cache_timeout=0), name="schema-json"),
+    path("swagger.yaml", schema_view.without_ui(cache_timeout=0), name="schema-yaml"),
 ]
