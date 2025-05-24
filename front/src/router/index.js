2a230e6cfad56c2259932b02bc91894d51c053de
SunshineMoonGit <sunshinemoongit@gmail.com>
2025-05-24 19:20:33 +0900

    hotfix

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import BaseLayout from '@/layouts/BaseLayout.vue'
 
+import ArticleView from '@/views/ArticleView.vue'
 import HomeView from '@/views/HomeView.vue'
 import LoginView from '@/views/LoginView.vue'
 import MapView from '@/views/MapView.vue'
